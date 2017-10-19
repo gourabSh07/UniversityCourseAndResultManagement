1becd78115bed9dd1d17b068d0759bc0bbe53409
gourab shingha <gourabshingha@gmail.com>
2017-10-19 13:16:43 +0600

    Added Student View Result

---
+++ UniversityCourseAndResultManagement/UniversityCourseAndResultManagement/BLL/ViewResultManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UniversityCourseAndResultManagement.DLL;
+using UniversityCourseAndResultManagement.Models;
+
+namespace UniversityCourseAndResultManagement.BLL
+{
+    public class ViewResultManager
+    {
+        ViewResultGateway resultGateway = new ViewResultGateway();
+        public List<ResultView> GetAllCourseCodeNameResultByStudentId(int studentId)
+        {
+            return resultGateway.GetAllCourseCodeNameResultByStudentId(studentId);
+        }
+    }
+}
